0f4152a0e3462c952deeb0b32b53ba602527d0a8
Tim Loden <tim.m.loden@gmail.com>
2019-04-16 12:41:02 -0700

    added wpgulp

--- functions.php
+++ functions.php
@@ -122,9 +122,13 @@ add_action( 'widgets_init', 'mccreerys_kiosk_widgets_init' );
 function mccreerys_kiosk_scripts() {
 	wp_enqueue_style( 'mccreerys-kiosk-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'mccreerys-kiosk-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+	// vendor scripts. only uncomment if you have files in assets/js/vendor
+	// wp_enqueue_script( 'demo-theme-vendor-scripts', get_template_directory_uri() . '/assets/js/vendor.min.js', array(), '20151215', true );
 
-	wp_enqueue_script( 'mccreerys-kiosk-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	// custom demo_theme_scripts
+	// note that we pass 'customize-preview' into the array();
+	wp_enqueue_script( 'demo-theme-custom-scripts', get_template_directory_uri() . '/assets/js/custom.min.js', array('customize-preview'), '20151215', true );
+
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
